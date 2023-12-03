7203bdf2ebfbf3c36dfc58d97c149b3163430936
BALA MURALI KRISHNA PENUMARTHI <Balu5a3@users.noreply.github.com>
2023-12-03 00:01:12 -0500

    Initial commit

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+
+        public int balu()
+        {
+            return 1;
+        }
+
+        public string balu(int i)
+        {
+            return "1";
+        }
+        //Only Different types of parameters and different no of parameters, Not Different types of return types.
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
